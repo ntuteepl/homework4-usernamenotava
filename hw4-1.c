ddb476f62baa3d244222d007e7c86b9bf02bb2f5
usernamenotava <146065210+usernamenotava@users.noreply.github.com>
2023-10-31 02:45:41 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 1 || n >= 50) {
+        printf("n is not inside 50\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+
+        if (isPrime(num)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
